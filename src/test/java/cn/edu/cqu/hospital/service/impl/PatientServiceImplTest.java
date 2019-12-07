ec1e4a9be300de59bfd527972a5c421e9dea30a8
JZX555 <505684228@qq.com>
2019-12-07 17:24:39 +0800

    add test case

---
+++ src/test/java/cn/edu/cqu/hospital/service/impl/PatientServiceImplTest.java
@@ -0,0 +1,28 @@
+package cn.edu.cqu.hospital.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.edu.cqu.hospital.pojo.Patient;
+import cn.edu.cqu.hospital.service.PatientService;
+
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
+@ContextConfiguration(locations = {"classpath:spring-context.xml"})  
+public class PatientServiceImplTest {
+	@Autowired
+	private PatientService patientService = null;
+	
+	@Test
+	public void test() {
+		System.out.println("begin");
+		Patient p;
+		p = this.patientService.getPatientByID("511112199803190913");
+		System.out.println(p.getPhone());
+	}
+
+}
