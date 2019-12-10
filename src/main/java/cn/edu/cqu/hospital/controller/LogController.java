13979b4428c983fde06c25796c327ba369384106
JZX555 <505684228@qq.com>
2019-12-10 15:04:59 +0800

    add validate method

--- src/main/java/cn/edu/cqu/hospital/controller/LogController.java
+++ src/main/java/cn/edu/cqu/hospital/controller/LogController.java
@@ -9,7 +9,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.edu.cqu.hospital.pojo.Chemist;
+import cn.edu.cqu.hospital.pojo.Collector;
+import cn.edu.cqu.hospital.pojo.Dispenser;
+import cn.edu.cqu.hospital.pojo.Doctor;
 import cn.edu.cqu.hospital.pojo.Patient;
+import cn.edu.cqu.hospital.service.ChemistService;
+import cn.edu.cqu.hospital.service.CollectorService;
+import cn.edu.cqu.hospital.service.DispenserService;
+import cn.edu.cqu.hospital.service.DoctorService;
 import cn.edu.cqu.hospital.service.PatientService;
 
 @Controller
@@ -17,6 +25,14 @@ import cn.edu.cqu.hospital.service.PatientService;
 public class LogController {
 	@Autowired
 	PatientService patientService = null;
+	@Autowired
+	DoctorService DoctorService = null;
+	@Autowired
+	CollectorService CollectorService = null;
+	@Autowired
+	ChemistService ChemistService = null;
+	@Autowired
+	DispenserService DispenserService = null;
 
 	@RequestMapping("/login")
 	public String login(HttpServletRequest request,Model model) {
@@ -49,6 +65,71 @@ public class LogController {
     	System.out.println("准备回到login");
 		return "login";
 	}	
+	
+	@RequestMapping("/validate")
+	public String validate(HttpServletRequest request, HttpServletResponse response, Model model) {
+		String ID = request.getParameter("ID");
+		String password = request.getParameter("password");
+		String userType = request.getParameter("userType");
+		
+		System.out.println(userType);
+		
+		if(userType.equals("patient")) {
+			Patient patient = this.patientService.getPatientByID(ID);
+			
+	    	if(patient != null) {
+	    		if(patient.getPassword().equals(password)) {	
+		    	    Cookie IDCookie = new Cookie("loginID", ID);  
+		    	    Cookie passwordCookie = new Cookie("loginPassword", password);  
+		    	    IDCookie.setMaxAge(60 * 60);  
+		    	    IDCookie.setPath("/");  
+		    	    passwordCookie.setMaxAge(60 * 60);  
+		    	    passwordCookie.setPath("/");  
+		    	    response.addCookie(IDCookie);  
+		    	    response.addCookie(passwordCookie); 
+		   
+		    	    model.addAttribute("patient", patient);
+		    		
+		            return "redirect:/"; 
+		    	}
+	    	}
+		}
+		
+		else if(userType.equals("doctor")) {
+			Doctor doctor = this.DoctorService.getDoctorByID(ID); 
+			if(doctor != null) {
+				if(doctor.getPassword().equals(password))
+					return "redirect:/";
+			}
+		}
+		
+		else if(userType.equals("chemist")) {
+			Chemist chemist = this.ChemistService.getChemistByID(ID);
+			if(chemist != null) {
+				if(chemist.getPassword().equals(password))
+					return "redirect:/";
+			}
+		}
+		
+		else if(userType.equals("dispenser")) {
+			Dispenser dispenser = this.DispenserService.getDispenserByID(ID);
+			if(dispenser != null) {
+				if(dispenser.getPassword().equals(password))
+					return "redirect:/";
+			}
+		}
+		
+		else if(userType.equals("collector")) {
+			Collector collector = this.CollectorService.getCollectorByID(ID);
+			if(collector != null) {
+				if(collector.getPassword().equals(password))
+					return "redirect:/";
+			}
+		}
+    	
+    	model.addAttribute("flag", 1);
+    	return "forward:/log/login";	
+	}
 	
 	@RequestMapping("/logout")
 	public String logout(HttpServletRequest request, HttpServletResponse response, Model model) {
