31dc761336aa2eb263e596ee93a9193a36825430
JZX555 <505684228@qq.com>
2019-12-14 19:25:43 +0800

    add patient_ID and remove date

--- src/main/java/cn/edu/cqu/hospital/pojo/UnPay.java
+++ src/main/java/cn/edu/cqu/hospital/pojo/UnPay.java
@@ -1,21 +1,29 @@
 package cn.edu.cqu.hospital.pojo;
 
 public class UnPay {
+	private String patient_ID;
 	private Integer item;
 	private String item_ID;
-	private String date;
 	private Double price;
 	
 	public UnPay() {
 		super();
 	}
 	
-	public UnPay(Integer item, String item_ID, String date, Double price) {
+	public UnPay(String patient_ID, Integer item, String item_ID, Double price) {
 		super();
+		this.patient_ID = patient_ID;
 		this.item = item;
 		this.item_ID = item_ID;
-		this.date = date;
 		this.price = price;
+	}
+
+	public String getPatient_ID() {
+		return patient_ID;
+	}
+
+	public void setPatient_ID(String patient_ID) {
+		this.patient_ID = patient_ID;
 	}
 
 	public Integer getItem() {
@@ -32,14 +40,6 @@ public class UnPay {
 
 	public void setItem_ID(String item_ID) {
 		this.item_ID = item_ID;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
 	}
 
 	public Double getPrice() {
