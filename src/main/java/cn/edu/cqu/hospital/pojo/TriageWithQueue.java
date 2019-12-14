65d04bae647162ff86a59f60c592effd76bd92d4
JZX555 <505684228@qq.com>
2019-12-14 17:57:27 +0800

    rewrite TriageWithQueue

--- src/main/java/cn/edu/cqu/hospital/pojo/TriageWithQueue.java
+++ src/main/java/cn/edu/cqu/hospital/pojo/TriageWithQueue.java
@@ -1,30 +1,73 @@
 package cn.edu.cqu.hospital.pojo;
 
 public class TriageWithQueue {
-	private Queue queue;
-	private Triage triage;
+	private String queue_ID;
+	private String register_ID;
+	private String doc_ID;
+	private String depart_ID;
+	private Integer index;
+	private Integer remained;
 	
 	public TriageWithQueue() {
 		super();
 	}
 	
-	public TriageWithQueue(Queue queue, Triage triage) {
+	public TriageWithQueue(String queue_ID, String register_ID, String doc_ID, String depart_ID, Integer index,
+			Integer remained) {
 		super();
-		
-		this.queue = queue;
-		this.triage = triage;
+		this.queue_ID = queue_ID;
+		this.register_ID = register_ID;
+		this.doc_ID = doc_ID;
+		this.depart_ID = depart_ID;
+		this.index = index;
+		this.remained = remained;
 	}
-	
-	public Queue getQueue() {
-		return queue;
+
+	public String getQueue_ID() {
+		return queue_ID;
 	}
-	public void setQueue(Queue queue) {
-		this.queue = queue;
+
+	public void setQueue_ID(String queue_ID) {
+		this.queue_ID = queue_ID;
 	}
-	public Triage getTriage() {
-		return triage;
+
+	public String getRegister_ID() {
+		return register_ID;
 	}
-	public void setTriage(Triage triage) {
-		this.triage = triage;
+
+	public void setRegister_ID(String register_ID) {
+		this.register_ID = register_ID;
+	}
+
+	public String getDoc_ID() {
+		return doc_ID;
+	}
+
+	public void setDoc_ID(String doc_ID) {
+		this.doc_ID = doc_ID;
+	}
+
+	public String getDepart_ID() {
+		return depart_ID;
+	}
+
+	public void setDepart_ID(String depart_ID) {
+		this.depart_ID = depart_ID;
+	}
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public Integer getRemained() {
+		return remained;
+	}
+
+	public void setRemained(Integer remained) {
+		this.remained = remained;
 	}
 }
