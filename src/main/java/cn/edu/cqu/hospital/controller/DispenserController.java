5e5e9a337bbbe3fe5c5faa8b5661fff83108df7f
JZX555 <505684228@qq.com>
2019-12-14 16:16:00 +0800

    add getAllMedicines

--- src/main/java/cn/edu/cqu/hospital/controller/DispenserController.java
+++ src/main/java/cn/edu/cqu/hospital/controller/DispenserController.java
@@ -1,5 +1,7 @@
 package cn.edu.cqu.hospital.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.edu.cqu.hospital.pojo.Medicine;
 import cn.edu.cqu.hospital.service.MedicineService;
 
 @Controller
@@ -24,5 +28,11 @@ public class DispenserController {
 	@RequestMapping("/MedList")
 	public String MedList(HttpServletRequest request, HttpServletResponse response, Model model) {
 		return "MedList";
+	}
+	
+	@RequestMapping("/getAllMedicines")
+	@ResponseBody
+	public List<Medicine> getAllMedicines(HttpServletRequest request, HttpServletResponse response, Model model) {
+		return this.medicinService.getAllMedicines();
 	}
 }
